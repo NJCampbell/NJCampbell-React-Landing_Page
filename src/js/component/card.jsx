4028192a48fa63d8b9902aa463ff2fd22f14c832
Nancy Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-07-29 15:16:09 +0000

    adjusted images

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -5,7 +5,7 @@ const Card = () => {
     return (
         <>
             <div className="card" style={{ width: "18rem" }}>
-                <img src="https://picsum.photos/100/100" className="card-img-top" alt="..." />
+                <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title d-flex justify-content-center">Card Title</h5>
                     <p className="card-text d-flex justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quam mollitia unde dolor ullam dicta, eum fugiat.</p>
